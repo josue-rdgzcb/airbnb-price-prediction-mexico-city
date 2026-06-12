c1e1de60bdca59771e26dd21b3ec3bc843a4306b
josue-rdgzcb <josue.rdgzcb@gmail.com>
2026-06-12 16:21:57 -0600

    refactor(settings): clean and restructure MODEL_FEATURES list

--- src/settings/model_config.py
+++ src/settings/model_config.py
@@ -2,21 +2,20 @@
 MODEL_FEATURES = [
 
     # Numeric features
-    'calculated_host_listings_count_entire_homes_log',
-    'calculated_host_listings_count_private_rooms_log',
-    'dist_to_nearest_attraction_log', 'beds_log', 'amenity_score',
-    'accommodates_log', 'bedrooms_log', 'bathrooms_log',
-    'attractions_within_radius_log', 'commercial_within_radius',
+    'calculated_host_listings_count_entire_homes',
+    'calculated_host_listings_count_private_rooms',
+    'dist_to_nearest_attraction', 'beds', 'amenity_score',
+    'accommodates', 'bedrooms', 'bathrooms',
+    'attractions_within_radius', 'commercial_within_radius',
 
     # Categorical features
-    'room_type_entire_home/apt','room_type_hotel_room', 'room_type_private_room','room_type_shared_room',
-    'neighbourhood_cleansed_freq', 'property_group_room_freq',
-    'host_total_listings_segment_ordinal', 'review_scores_mean_segment_ordinal', 
-    'minimum_nights_segment_ordinal', 'host_verifications_grouped_ordinal',
+    'room_type','neighbourhood_cleansed', 'property_group_room',
+    'host_total_listings_segment', 'review_scores_mean_segment', 
+    'minimum_nights_segment', 'host_verifications_grouped',
 
     # Boolean features
     'host_is_superhost','instant_bookable', 
     'has_tv', 'has_elevator', 'has_free_parking', 'has_coffee_maker', 
-    'has_outdoor_furniture', 'has_air_conditioning','has_self_check_in', 'has_pool',
+    'has_outdoor_furniture', 'has_air_conditioning','has_self_check_in', 'has_pool'
     
 ]
